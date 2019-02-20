70f15aed23fd51ad9e04cd4889a3eb717705201a
fhendricks2 <f.rubymelody@gmail.com>
2019-02-20 11:51:38 +0200

    display data types

---
+++ Display Data Types/js/main.js
@@ -0,0 +1,44 @@
+// *=====Array=====* //
+var cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+console.log(cars);
+
+// *=====Object=====* //
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+console.log(person);
+
+// *=====String=====* //
+var x = "My name is Fatima Hendricks";
+console.log(x);
+
+// *=====Number=====*//
+var a = 10;  
+var b = 1.555;   
+console.log(a);
+console.log(b);
+
+// *=====Boolean=====* //
+var boolean = 10 > 9;
+console.log(boolean);
+
+// *=====Null=====* //
+var number = null;
+console.log(number);
+
+// *=====Undefined=====* //
+var name;
+console.log(name);
+
+// *=====Date=====* //
+var d = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(d);
+
+
